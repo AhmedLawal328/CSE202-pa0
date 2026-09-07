6e19e1fae5471ea1ba50f8e67b6e09fa3445e747
Ahmed <alawalyork@gmail.com>
2026-09-06 21:31:21 -0400

    refactoring

--- prog0.c
+++ prog0.c
@@ -52,9 +52,38 @@ unsigned rotate_right(unsigned x, int n){
 // returns x+y if no overflow occurs
 // returns TMAX if a positive overflow occurs
 // returns TMIN if a negative overflow occurs
-int saturating_add(int x, int y);
+int saturating_add(int x, int y){
+
+    int sum = x + y;
+    
+    // Check if they're both positive and sum became nonnegative
+    if ((x >> 31) == 0 && (y >> 31) == 0 && (sum >> 31) != 0) {
+        return 0x7fffffff;   // TMAX
+    }
+
+    /// Check if they're both negative and sum became positve   
+    if ((x >> 31) != 0 && (y >> 31) != 0 && (sum >> 31) == 0) {
+        return 0x80000000;   // TMIN
+    }
+    return sum;
+}
+
+
 // multiplies the binary representation of a float number f by 2
-unsigned float_twice(unsigned f);
+unsigned float_twice(unsigned f){
+
+    //seperate the exp
+    unsigned exp = (f & 0x7f800000) >> 23;
+
+    // Check if exp is all 1s
+    if (exp == 0xff) {
+        return f;
+    }
+
+    // Normalized number
+    return f + 0x00800000;
+
+}
 // divides the binary representation of a float number f by 2
 unsigned float_half(unsigned f);
 
@@ -65,37 +94,71 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    union value v;
-    v.uval = strtoul(argv[2], NULL, 16);
-    if (v.uval == 0){
-        printf("Invalid hex value");
+    union value v;  
+    char *end;
+
+    v.uval = strtoul(argv[2], &end, 16);
+
+    if (*end != '\0') {
+        printf("Invalid hex value\n");
         return 1;
     }
 
     if (strcmp(argv[1], "even") == 0) {
-     
+
         int num = any_even_one(v.uval);
-        printf("%s\n", num ? "True" : "False"); 
+        printf("%s\n", num ? "True" : "False");
 
     } else if (strcmp(argv[1], "left") == 0) {
-        int num = leftmost_one(v.uval);
+
+        unsigned num = leftmost_one(v.uval);
         printf("%x\n", num);
 
     } else if (strcmp(argv[1], "rrotate") == 0) {
+
         // Get digit by subtracting '0'
-        int n = argv[3][0] - '0';
-        int num = rotate_right(v.uval, n);
-        printf("%x", num);
+        char *end2;
+        int n = strtoul(argv[3], &end2, 10);
+
+        if (*end2 != '\0' || n < 0 || n > 31) {
+            printf("Invalid number of shift positions\n");
+            return 1;
+        }
+
+        unsigned num = rotate_right(v.uval, n);
+        printf("%x\n", num);
 
     } else if (strcmp(argv[1], "lrotate") == 0) {
-        int n = argv[3][0] - '0';
-        int num = rotate_left(v.uval, n);
-        printf("%x", num);
+
+        // Get digit by subtracting '0'
+        char *end2;
+        int n = strtoul(argv[3], &end2, 10);
+
+        if (*end2 != '\0' || n < 0 || n > 31) {
+            printf("Invalid number of shift positions\n");
+            return 1;
+        }
+
+        unsigned num = rotate_left(v.uval, n);
+        printf("%x\n", num);
 
     } else if (strcmp(argv[1], "saturate") == 0) {
 
+        char *end2;
+        int sec_arg = (int) strtoul(argv[3], &end2, 16);
+
+        if (*end2 != '\0') {
+            printf("Invalid hex value\n");
+            return 1;
+        }
+
+        int num = saturating_add((int) v.uval, sec_arg);
+        printf("%d\n", num);
+
     } else if (strcmp(argv[1], "twice") == 0) {
 
+        unsigned num = float_twice(v.uval);
+        printf("%x\n", num);
 
     } else if (strcmp(argv[1], "half") == 0) {
 
